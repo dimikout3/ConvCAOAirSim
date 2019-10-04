84dd4e01be77a4c0ae9350d217de7833423cc407
Dimitrios Koutras <dimikout3@gmail.com>
2019-10-04 11:28:02 +0300

    Generating a video with each drones depth perspective

--- videoDronePerspective.py
+++ videoDronePerspective.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     for droneIdx, drone in enumerate(dronesID):
         print(f"\n[DRONE]: {drone}")
 
-        out = cv2.VideoWriter(f"{drone}_detections.avi", fourcc, 1.0, (2048,1024))
+        out = cv2.VideoWriter(f"{drone}_perspective_depth.avi", fourcc, 1.0, (2048,1024))
 
         for positionIdx, position in enumerate(wayPointsID):
             print(f"{4*' '}[POSITION]: {position}")
@@ -36,11 +36,20 @@ if __name__ == "__main__":
             for time,time_step in enumerate(time_steps):
 
                 raw_image_path = os.path.join(current_raw_dir, f"scene_time_{time}.png")
-                detected_image_path = os.path.join(current_detected_dir, f"detected_time_{time}.png")
+                depth_image_path = os.path.join(current_raw_dir, f"depth_time_{time}.pfm")
 
                 frame_raw = cv2.imread(raw_image_path)
-                frame_detected = cv2.imread(detected_image_path)
+                frame_detected,s = airsim.read_pfm(depth_image_path)
 
+                # cut values above 100 meters
+                frame_detected[frame_detected>100] = 100.
+                # convert from 0->255 fro greyscale
+                frame_detected = (frame_detected/100.) * 255.
+                # save it
+                file_out = os.path.join(current_raw_dir, f"depth_scene_time_{time}.png")
+                cv2.imwrite(file_out,frame_detected)
+
+                frame_detected = cv2.imread(file_out)
                 fram_vertical = np.concatenate((frame_raw,frame_detected), axis=1)
                 # h, w, _ = fram_vertical.shape
                 # print(f" width:{w} -- height:{h}")
