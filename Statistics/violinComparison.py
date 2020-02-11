dcc11fb9a8faa571d53787c989a7178cfc1e0a94
Dimitrios Koutras <dimikout3@gmail.com>
2020-02-11 10:09:47 +0200

    Clean violin plots

--- Statistics/violinComparison.py
+++ Statistics/violinComparison.py
@@ -8,10 +8,7 @@ plotstyle="ggplot"
 # sns.set_style(f"{plotstyle}")
 plt.style.use(f"{plotstyle}")
 
-COMPARISON_PLOT = "violin"
-# COMPARISON_PLOT = "catplot"
-
-COMPARE = ['Drones_2.p','Drones_3.p','Drones_4.p']
+COMPARE = ['UAVs 2.p','UAVs 3.p','UAVs 4.p', 'UAVs 5.p']
 
 if __name__ == "__main__":
 
@@ -24,13 +21,8 @@ if __name__ == "__main__":
 
     dataFrame = pd.concat(dataFrameList)
 
-    if COMPARISON_PLOT == "violin":
-        # sns.violinplot(x="Drones Number", y="Confidence Interval",inner=None, data=dataFrame[dataFrame['Time[sec]']>150*3])
-        # sns.violinplot(x="Drones Number", y="Confidence Interval",size=0.03, data=dataFrame[dataFrame['Time[sec]']>150*3])
-        g = sns.catplot(x="Drones Number", y="Confidence Interval",kind="violin",inner=None, data=dataFrame[dataFrame['Time[sec]']>150*3])
-        sns.swarmplot(x="Drones Number", y="Confidence Interval", color="k", size=0.4, data=dataFrame, ax=g.ax)
-    elif COMPARISON_PLOT == "catplot":
-        sns.catplot(x="Drones Number", y="Confidence Interval",kind="swarm",data=dataFrame)
+    g = sns.catplot(x="UAV Number", y="Confidence Interval",kind="violin",inner=None, data=dataFrame[dataFrame['Time[sec]']>150*3])
+    sns.swarmplot(x="UAV Number", y="Confidence Interval", color="k", size=0.4, data=dataFrame, ax=g.ax)
 
     # plt.show()
-    plt.savefig(f"Compare_{COMPARISON_PLOT}_{plotstyle}.png")
+    plt.savefig(f"Compare.png")
