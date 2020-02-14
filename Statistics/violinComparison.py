9ace9653584db7b326151a596dfdcc1c34ed12e8
Dimitrios Koutras <dimikout3@gmail.com>
2020-02-14 13:53:02 +0200

    ViolinPlot for 6 UAVs

--- Statistics/violinComparison.py
+++ Statistics/violinComparison.py
@@ -8,7 +8,7 @@ plotstyle="ggplot"
 # sns.set_style(f"{plotstyle}")
 plt.style.use(f"{plotstyle}")
 
-COMPARE = ['UAVs 2.p','UAVs 3.p','UAVs 4.p', 'UAVs 5.p']
+COMPARE = ['2 UAVs.p','3 UAVs.p','4 UAVs.p', '5 UAVs.p', '6 UAVs.p']
 
 if __name__ == "__main__":
 
@@ -20,9 +20,9 @@ if __name__ == "__main__":
         file.close()
 
     dataFrame = pd.concat(dataFrameList)
-
-    g = sns.catplot(x="UAV Number", y="Confidence Interval",kind="violin",inner=None, data=dataFrame[dataFrame['Time[sec]']>150*3])
-    sns.swarmplot(x="UAV Number", y="Confidence Interval", color="k", size=0.4, data=dataFrame, ax=g.ax)
+    
+    g = sns.catplot(x="UAV Number", y="Objective Function",kind="violin",inner=None, data=dataFrame[dataFrame['Time Steps']>150])
+    sns.swarmplot(x="UAV Number", y="Objective Function", color="k", size=0.4, data=dataFrame, ax=g.ax)
 
     # plt.show()
     plt.savefig(f"Compare.png")
