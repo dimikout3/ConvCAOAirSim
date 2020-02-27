0e379997abcf3bba3d6f9cba8fbeb51a8e277700
Dimitrios Koutras <dimikout3@gmail.com>
2020-02-27 11:47:14 +0200

    Batch video generator

---
+++ Videos/makeVideos.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import numpy as np
+import pickle
+import cv2
+from tqdm import tqdm
+
+import sceneVideo
+import optparse
+import heatVideo
+import pointCloud
+
+
+""" Launches all the videos to be generated"""
+
+
+def get_options():
+
+    optParser = optparse.OptionParser()
+    optParser.add_option("--scene",action="store_true", default=False, dest="scene", help="UAVs scene image going to be generated")
+    optParser.add_option("--heat",action="store_true", default=False, dest="heat", help="UAVs depth image going to be generated")
+    optParser.add_option("--pointCloud",action="store_true", default=False, dest="pointCloud", help="UAVs 3D image going to be generated")
+    options, args = optParser.parse_args()
+
+    return options
+
+
+if __name__ == "__main__":
+
+    options = get_options()
+
+    simulation_dir = os.path.join(os.getcwd(), "..","results_pointCloud")
+
+    if options.scene:
+        sceneVideo.generate(path=simulation_dir, time=60)
+
+    if options.heat:
+        # TODO: check if depth.PNG exists, else generate it (heatGenerated.py)
+        heatVideo.generate(path=simulation_dir, time=60)
+
+    if options.pointCloud:
+        # TODO: check if depth.PNG exists, else generate it (heatGenerated.py)
+        pointCloud.generate(path=simulation_dir, time=60)
