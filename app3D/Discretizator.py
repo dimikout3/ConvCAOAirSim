922dfa78058ae65e203dcfc92c2057f60f8d008c
dimikout3 <dimikout3@gmail.com>
2020-03-13 12:48:16 +0200

    Helper object for making continues space discrete

---
+++ app3D/Discretizator.py
@@ -0,0 +1,27 @@
+class Discretizator:
+
+    def __init__(self, discrete=None, geofence=None):
+
+        if discrete == None or geofence == None:
+            print("[ERROR] Discretizator need geofence and discrete steps !!")
+            return 0
+
+        self.discreteX = discrete['x']
+        self.discreteY = discrete['y']
+        self.discreteZ = discrete['z']
+
+        self.highX, self.highY, self.highZ = geofence.getHighValues()
+        self.lowX, self.lowY, self.lowZ = geofence.getLowValues()
+
+        self.stepX = (self.highX - self.lowX)/self.discreteX
+        self.stepY = (self.highY - self.lowY)/self.discreteY
+        self.stepZ = (self.highZ - self.lowZ)/self.discreteZ
+
+
+    def report(self):
+        
+        print("Discretizator has ")
+        print(f"discreteX={self.discreteX} discreteY={self.discreteY} discreteZ={self.discreteZ}")
+        print(f"highX={self.highX} highY={self.highY} highZ={self.highZ}")
+        print(f"lowX={self.lowX} lowY={self.lowY} lowZ={self.lowZ}")
+        print(f"stepX={self.stepX} stepY={self.stepY} stepZ={self.stepZ}")
