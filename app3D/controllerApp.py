2ac31e4bf6f6a0d133d5f38261515e7ebf57b2af
dimikout3 <dimikout3@gmail.com>
2020-03-16 14:36:59 +0200

    Intermediate Functions

--- app3D/controllerApp.py
+++ app3D/controllerApp.py
@@ -16,3 +16,24 @@ class controllerApp(controller):
         super().__init__(clientIn, droneName, offSets, ip=ip,
                          wayPointsSize = wayPointsSize,
                          estimatorWindow = estimatorWindow)
+
+        self.descretePointCloud = []
+
+
+    def pointCloud2Descrete(self, discretizator=None):
+
+        x, y, z, colors = self.pointCloud[-1]
+
+        data = np.stack((x,y,z),axis=1)
+
+        self.descretePointCloud.append( discretizator.descrete(data) )
+
+
+    def connectIntermidiate(self):
+        """Determine where the points between last descrete and UAV belong
+           (Obstacles, Explored)"""
+        pass
+
+
+    def quit(self):
+        pass
